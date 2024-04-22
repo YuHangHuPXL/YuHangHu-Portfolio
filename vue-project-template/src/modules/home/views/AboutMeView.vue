9cfc51f3528a9e87c4fb06cc7c15723e9e88c681
Yu Hang Hu <yuhang.hu@student.pxl.be>
2024-04-22 21:42:28 +0200

    About me

--- vue-project-template/src/modules/home/views/AboutMeView.vue
+++ vue-project-template/src/modules/home/views/AboutMeView.vue
@@ -6,7 +6,7 @@ import AppPage from '@/components/app/AppPage.vue'
 
 <template>
 	<AppPage title="Wie ben ik?">
-		<div class="flex w-full flex-col">
+		<div class="flex w-full flex-col gap-y-5">
 			<div class="mx-auto w-1/2 pb-5">
 				<AppIcon
 					icon="quotes"
@@ -30,18 +30,9 @@ import AppPage from '@/components/app/AppPage.vue'
 			<AppText
 				class="leading-loose"
 				variant="body"
-				>Dit is wat ik altijd tegen mezelf vertel als het te moeilijk wordt. Een gezegde met een betekenis dat moeilijke
-				tijden nodig kunnen zijn voor groei en dat je deze uitdagingen rechtstreeks aangaat. Ik ben Yu Hang Hu, een
-				laatstejaars student van de richting Toegepaste Informatica aan de Hogeschool PXL. In het middelbaar had ik nog
-				geen besef wat ik later wil worden maar ik had altijd een interesse in de menselijke lichaam en informatica.
-				Omdat ik niet kon kiezen heb ik dus de advies van mijn leerkrachten gevolgd om iets wetenschappelijks te doen.
-				Ik ben hierna begonnen aan de opleiding Medische laboratoriumtechnologie aan de Hogeschool UCLL. Omdat ik niet
-				genoeg voldoening had na deze opleiding heb ik ook het schakelprogramma gedaan naar een master Biomedische
-				wetenschappen aan de Hasselt. Uiteindelijk kreeg ik een grote besef dat wetenschap uiteindelijk niet mijn passie
-				was.
+				>Dit is wat ik altijd tegen mezelf zeg als het moeilijk wordt. Dit gezegde betekent dat moeilijke tijden noodzakelijk kunnen zijn voor groei en dat je deze uitdagingen rechtstreeks moet aangaan. Mijn naam is Yu Hang Hu, en ik ben een laatstejaarsstudent Toegepaste Informatica aan    de Hogeschool PXL. Tijdens mijn middelbare schooltijd wist ik nog niet wat ik later wilde worden, maar ik had altijd al interesse in het menselijk lichaam en informatica. Omdat ik geen keuze kon maken, heb ik het advies van mijn leraren opgevolgd om iets wetenschappelijks te doen. Daarom begon ik aan de opleiding Medische Laboratoriumtechnologie aan Hogeschool UCLL. Omdat deze opleiding mij niet genoeg voldoening gaf, heb ik ook het schakelprogramma naar een master in Biomedische Wetenschappen aan de Universiteit Hasselt gevolgd. Uiteindelijk realiseerde ik me dat wetenschap niet mijn passie was en was ik hiermee ook gestopt.
 			</AppText>
 			<br />
-
 			<div class="mx-auto flex items-center gap-x-9 py-4">
 				<img
 					alt="ucll"
@@ -64,16 +55,13 @@ import AppPage from '@/components/app/AppPage.vue'
 				class="leading-loose"
 				variant="body"
 			>
-				Hierna ben ik geland op de Hogeschool PXL waar mijn passie voor programmeren is gestart. Vanaf de eerste lessen
-				had ik door dat dit iets is waarin ik de rest ik mijn leven wil aan spenderen. Het concept van een idee te
-				hebben en dit te maken van niks intrigeert mij enorm. Hetzelfde voor het schrijven van algoritmes en oplossingen
-				zoeken van problemen. Mijn ambities later is om mijn hard skills verder te ontwikkelen tot een hoog niveau.
-				Later in het werkveld wil ik gezien worden als een ‘goede’ meeloper. Mijn sterke punten zijn o.a. hardwerkend,
-				geduld, zelfstandigheid en stressbestendigheid. Deze heb ik leren kennen door na te denken over mijn
-				kernkwadranten. Via een scan van Talent heb ik geleerd dat ik meer een doen persoon ben dan een denk persoon.
-				Dit was tijdens het Research project vrij duidelijk toen ik direct bezig was met de uitvoering. Hierna heb ik
-				dan ook gewerkt aan mijn andere punten zoals beschreven in de onderstaande figuur.
+        Vervolgens ben ik terechtgekomen aan Hogeschool PXL, waar mijn passie voor programmeren begon. Vanaf de eerste lessen wist ik dat dit iets was waar ik de rest van mijn leven mee bezig wilde zijn. Het concept van het hebben van een idee en dit uit het niets creëren fascineert me enorm. Dit geldt ook voor het schrijven van algoritmen en het zoeken naar oplossingen voor bepaalde problemen. Mijn ambitie is om mijn technische vaardigheden verder te ontwikkelen tot een hoog niveau. Later in mijn carrière wil ik bekend staan als een 'goede' developer.
 			</AppText>
+      <AppText
+          class="leading-loose"
+          variant="body">
+        Mijn sterke punten zijn onder andere hardwerkend zijn, geduld hebben, zelfstandig zijn en stressbestendig zijn. Deze kwaliteiten heb ik leren kennen door na te denken over mijn kernkwaliteiten. Via een Thalento Scan heb ik geleerd dat ik meer een doener ben dan een denker. Dit werd duidelijk tijdens het onderzoeksproject, waarbij ik direct met de uitvoering begon. Vervolgens heb ik gewerkt aan het verbeteren van mijn andere punten zoals beschreven in de onderstaande figuur.
+      </AppText>
 
 			<img
 				alt="kern-kwadranten"
@@ -84,12 +72,9 @@ import AppPage from '@/components/app/AppPage.vue'
 			<AppText
 				class="pt-4 leading-loose"
 				variant="body"
-				>Dit was ook duidelijk in de IT-project. Ik heb meer tijd gespendeerd aan het brainstormen en in de volgende
-				analyse kwam ik meer uit als een denker. Uit dit project heb ik ook ontdekt dat ik ook deftig een leider kan
-				zijn en een samenwerking tot een goede eind kan brengen. Natuurlijk heb ik ook nog punten waaraan ik kan werken.
-				Een voorbeeld hiervan is dat ik soms te geïsoleerd ben wanneer ik hard aan het werken. Dit leidt dan tot een
-				verminderde communicatie tegenover mijn collega’s.
-			</AppText>
+				>Dit was eveneens duidelijk tijdens het IT-project. Ik heb meer tijd besteed aan het brainstormen en uit de daaropvolgende analyse bleek dat ik meer een denker ben geworden. Uit dit project heb ik ook ontdekt dat ik een effectieve leider kan zijn en een samenwerking succesvol kan afronden.
+        Natuurlijk heb ik ook nog verbeterpunten. Zo merk ik dat ik soms te geïsoleerd werk wanneer ik hard bezig ben, wat dan leidt tot verminderde communicatie met mijn collega’s. Dit is iets waar ik aan kan werken om mijn teamvaardigheden en algehele professionele skills te verbeteren.
+      </AppText>
 		</div>
 	</AppPage>
 </template>
